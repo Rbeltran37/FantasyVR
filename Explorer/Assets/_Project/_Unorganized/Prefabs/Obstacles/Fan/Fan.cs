e9e0ee2de10b98772a332ada7c350f7531fe94f0
Roberto Beltran <rbeltran37@gmail.com>
2022-12-01 13:25:39 -0500

    Reupload 2

---
+++ Explorer/Assets/_Project/_Unorganized/Prefabs/Obstacles/Fan/Fan.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    public bool isActivated = false;
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isActivated)
+        {
+            transform.Rotate(-transform.forward * speed);
+        }
+    }
+}
---
+++ Explorer/Assets/_Project/_Unorganized/Prefabs/Obstacles/Pendulum/Pendulum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SocialPlatforms;
+
+public class Pendulum : MonoBehaviour {
+    
+    [Range(0f, 360f)]
+    public float angle;
+    
+    [Range(0f, 5f)]
+    public float speed;
+
+    [Range(0f, 10f)]
+    public float startTime = 0.0f;
+    
+    
+    private Quaternion start, end;
+
+    private void Start()
+    {
+        start = PendulumRotation(angle);
+        end = PendulumRotation(-angle);
+    }
+
+    private void FixedUpdate()
+    {
+        startTime += Time.deltaTime;
+        transform.rotation = Quaternion.Lerp(start, end, (Mathf.Sin(startTime * speed + Mathf.PI / 2) + 1.0f) / 2.0f);
+    }
+
+    void ResetTimer()
+    {
+        startTime = 0.0f;
+    }
+
+    Quaternion PendulumRotation(float angle)
+    {
+        var pendulumRotation = transform.rotation;
+        var angleZ = pendulumRotation.eulerAngles.z + angle;
+
+        if (angleZ > 180)
+        {
+            angleZ -= 360;
+        }
+        else if (angleZ < -180)
+        {
+            angleZ += 360;
+        }
+        
+        pendulumRotation.eulerAngles = new Vector3(pendulumRotation.eulerAngles.x, pendulumRotation.y, angleZ);
+        return pendulumRotation;
+    }
+}
