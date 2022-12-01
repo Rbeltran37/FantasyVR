4053b7cc1837bb626435d135914ffe039390c04b
Roberto Beltran <rbeltran37@gmail.com>
2022-12-01 15:24:02 -0500

    Reupload 5

---
+++ Explorer/Assets/Thirdparty/_BowAndArrow/Scripts/OculusInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class OculusInput : MonoBehaviour
+{
+    public BowInstance mBowInstance = null;
+    public GameObject m_OppositeController = null;
+    public OVRInput.Controller m_Controller = OVRInput.Controller.None;
+
+    private void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+            mBowInstance.Pull(m_OppositeController.transform);
+
+        if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger, m_Controller))
+            mBowInstance.Release();
+    }
+}
